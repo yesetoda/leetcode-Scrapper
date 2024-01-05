9e56923e6dad9462893f95db4581f63e69c25da8
yesetoda <154346115+CamelsCase@users.noreply.github.com>
2024-01-05 07:48:23 -0300

    updated the spider to check for valid username

--- leetcodeScrapper/leetcodeScrapper/settings.py
+++ leetcodeScrapper/leetcodeScrapper/settings.py
@@ -17,7 +17,8 @@ NEWSPIDER_MODULE = "leetcodeScrapper.spiders"
 #USER_AGENT = "leetcodeScrapper (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+# ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- leetcodeScrapper/leetcodeScrapper/spiders/leetcodespider.py
+++ leetcodeScrapper/leetcodeScrapper/spiders/leetcodespider.py
@@ -6,20 +6,35 @@ class LeetcodespiderSpider(scrapy.Spider):
     allowed_domains = ["leetcode.com"]
     username = input("enter the username oof the profile : ")
     start_urls = [f"https://leetcode.com/{username}/"]
+    
+    def start_requests(self):
+        while True:
+            username = input("Enter the username of the profile: ")
+            url = f"https://leetcode.com/{username}/"
+            yield scrapy.Request(url=url, callback=self.parse)
 
+            # Check if the username is valid
+            if self.is_valid_username(username):
+                break
+            else:
+                print("Invalid username. Please try again.")
+    
     def parse(self, response):
-        
         rank = "".join( response.css(".ttext-label-1::text").extract())
         total = "".join(response.css(".text-\[24px\]::text").extract())
         nickname = "".join( response.css(".break-all::text").extract())
         fullname = "".join( response.css(".lc-lg\:w-\[300px\] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)::text").extract())
+        fullname = "unknown" if not fullname else fullname
         hard = response.css("div.space-y-2:nth-child(3) > div:nth-child(1) > div:nth-child(2) > span:nth-child(1)::text").extract()[0] + "".join(response.css('div.space-y-2:nth-child(3) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2)::text').extract())
         medium = response.css("div.space-y-2:nth-child(2) > div:nth-child(1) > div:nth-child(2) > span:nth-child(1)::text").extract()[0] + "".join(response.css('div.space-y-2:nth-child(2) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2)::text').extract())
         easy = response.css(".lc-xl\:max-w-\[228px\] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > span:nth-child(1)::text").extract()[0] + "".join(response.css('.lc-xl\:max-w-\[228px\] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2)::text').extract())
         languages = ",".join(response.css(".notranslate::text").extract())
-
+        if not nickname and not fullname:
+            print("this username is not correct try again")
+            
+            
         # Write the data to a text file
-        with open('output.txt', 'a') as file:
+        with open('output.word', 'a') as file:
             file.write(f"\nFull Name: {fullname}\n")
             file.write(f"Nickname: {nickname}\n")
             file.write(f"Rank: {rank}\n")
@@ -29,4 +44,3 @@ class LeetcodespiderSpider(scrapy.Spider):
             file.write(f"Hard: {hard}\n")
             file.write(f"Languages: {languages}\n")
         add_record_if_not_exists(fullname, nickname, rank, easy, medium, hard, languages,total)
-        
